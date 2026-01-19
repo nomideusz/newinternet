8f9ed3f7dd17149bd84651980c910dcc76ace1a6
nomideusz <b.dymet@gmail.com>
2026-01-19 18:54:31 +0100

    Add application.js entrypoint for non-Inertia pages

---
+++ app/frontend/entrypoints/application.js
@@ -0,0 +1,8 @@
+// Entry point for non-Inertia pages (ERB/Turbo pages)
+// This file is loaded by application.html.erb layout
+
+// Import ActionCable for real-time features
+import "@rails/actioncable";
+
+// You can add any JavaScript needed for ERB/Turbo pages here
+console.log("Application entrypoint loaded");
